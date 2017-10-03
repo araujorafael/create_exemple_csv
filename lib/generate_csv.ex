00210facc7933149425de1b7f95e6dd9f8dcd22c
Rafael Araujo dos Santos <rafael.a.asantos@hsm.com.br>
2017-10-03 14:47:06 -0300

    refactory for more legible code

--- lib/generate_csv.ex
+++ lib/generate_csv.ex
@@ -22,7 +22,7 @@ defmodule GenerateCsv do
       # set random seed
       :rand.seed(:exs1024, :erlang.timestamp())
 
-      # showd create a array with each line of CSV format {id, course name, room number}
+      # showd create a array with each line of CSV format {id, class name, room number}
       create([], count)
   end
 
@@ -54,22 +54,32 @@ defmodule GenerateCsv do
   @spec create(integer(), atom()) :: String.t()
   def rand_str(size, type) do
     basic_alphabet = "abcdefghijklmnopqrstuvwxyz"
+
     alphabet = case type do
       :alphanumerical -> Enum.join(["0123456789", basic_alphabet], "")
       :char -> basic_alphabet
     end
 
-    rand_str(size, "", alphabet)
+    rand_str("", "", alphabet, size)
   end
 
-  defp rand_str(0, str, _alphabet) do
+  defp rand_str(_new_char, str, _alphabet, 0) do
     str
   end
 
-  defp rand_str(size, str, alphabet) do
-    rand_pos = :rand.uniform( String.length(alphabet) )
-    new_char = String.at(alphabet, rand_pos)
-    rand_str(size - 1, "#{str}#{new_char}", alphabet)
+  defp rand_str(new_char, str, alphabet, size) do
+    # Set the new string
+    str = "#{str}#{new_char}"
+
+    # Set the next random position
+    rand_pos = alphabet
+    |> String.length
+    |> :rand.uniform
+
+    # Recursive call with new letter that shold be added to string
+    alphabet
+    |> String.at(rand_pos)
+    |> rand_str(str, alphabet, size - 1)
   end
 
   @doc """
@@ -83,13 +93,25 @@ defmodule GenerateCsv do
   """
   @spec new_line() :: String.t()
   def new_line() do
-    # id
+    # Generate id
     id = :rand.uniform(100000)
-    # rand_room_number
-    room_number = rand_str(6, :alphanumerical)
-    # course name
-    course_name = "#{rand_str(4, :char)} #{:rand.uniform(1000)}"
 
-    Enum.join([id, course_name, room_number], ",")
+    # Generate randon room number
+    room_number = rand_str(6, :alphanumerical)
+
+    # Set pool of class names
+    class_name_pool = ["english 008", "portuguese 009", "math 001", "spanish 003"]
+
+    # Set the size of names pool
+    size = class_name_pool
+    |> Enum.count
+    |> Kernel.-(1)
+
+    # Select class name
+    class_name = class_name_pool
+    |> Enum.at(:rand.uniform(size))
+
+    # Join everything in one line
+    Enum.join([id, class_name, room_number], ",")
   end
 end
